1e8cb5aab82c30ac8be05706e9e14f815d9a3d80
rogelio <rromaflores@gmail.com>
2015-10-15 20:57:29 -0500

    diseño de la pagina principal de la aplicacion

---
+++ Aplicacion/AgendaViaje/AgendaViaje/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AgendaViaje.Resources;
+
+namespace AgendaViaje
+{
+    /// <summary>
+    /// Proporciona acceso a los recursos de cadena.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
