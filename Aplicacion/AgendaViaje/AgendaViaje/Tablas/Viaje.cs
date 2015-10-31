28b066e52839936b4cc99f2dd0f5958802862537
rogelio <rromaflores@gmail.com>
2015-10-30 20:11:29 -0500

    implementacion de la clase Viaje.cs

--- Aplicacion/AgendaViaje/AgendaViaje/Tablas/Viaje.cs
+++ Aplicacion/AgendaViaje/AgendaViaje/Tablas/Viaje.cs
@@ -1,12 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Data.Linq.Mapping;
+using System.Data.Linq;
+using System.ComponentModel;
+using AgendaViaje.VstaModelo.Base;
 
 namespace AgendaViaje.Tablas
 {
-    class Viaje
+    [Table(Name = "Viajes")]
+    public class Viaje : INotifyPropertyChanging
     {
+        private int viajeId;
+        private String nombre;
+        private DateTime fechaIni;
+        private DateTime fechaFin;
+        private int duracion;
+        private String presupuesto;
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int ViajeId
+        {
+            get
+            {
+                return viajeId;
+            }
+            set
+            {
+                viajeId = value;
+                this.OnPropertyChanging("ViajeId");
+                NotificarCambio("ViajeId");
+            }
+        }
+        [Column(CanBeNull = false)]
+        public String Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+                this.OnPropertyChanging("Nombre");
+                NotificarCambio("Nombre");
+
+            }
+        } 
     }
 }
